8916a6b0f6596c1411a5e5108aa243a2b19270ce
marionlilian <marionwangui49@gmail.com>
2025-08-07 18:11:52 +0300

    js code

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const transactions =[
     {
         id:Date.now(),
@@ -5,11 +6,11 @@ const transactions =[
         amount:3500,
         type:'income'
 
-    }
+    },
     {
         id:Date.now(),
-        decription:'Freelance Writing',
-        amount:-500,
+        description:'Freelance Writing',
+        amount:-1500,
         type:'expense'
     }
 ]
@@ -28,21 +29,85 @@ constructor(){
 
 loadTransactions(){
    console.log(transactions);
-    return transactions;
+    return JSON.parse(localStorage.getItem('transactions')) || [];
+}
+saveTransactions(){
+    localStorage.setItem('transactions', JSON.stringify(this.transactions))
 }
 
-initEventListeners(){
-
+initEventListeners() {
+  this.form.addEventListener('submit', e => {
+    e.preventDefault();
+    this.addTransaction();
+  });
 }
+
+addTransaction(){
+    const description =document.getElementById('description').value.trim();
+    const amount = parseFloat(document.getElementById('amount').value);
+    const type =document.getElementById('type').value;
+
+    if(!description || isNaN(amount)){
+        alert('Please provide a valid description and amount');
+        return;
+    }
+    const transaction ={
+        id:Date.now(),
+        description,
+        amount: type==='expense'? -amount: amount,
+        type
+    }
+    this.transactions.push(transaction);
+    this.saveTransactions();
+    this.renderTransactions();
+    this.updateBalance();
+    this.clearForm();
+    
+    };
+  clearForm() {
+  document.getElementById('description').value = "";
+  document.getElementById('amount').value = "";
+}
+
 renderTransactions(){
+    this.transactionList.innerHTML ='';
+    this.transactions
+    .slice()
+    .sort((a, b)=>b.id- a.id)
+    .forEach((transactions)=> {
+        const transactionDiv =document.createElement('div');
+        transactionDiv.classList.add('transaction', transactions.type);
+        transactionDiv.innerHTML = `
+        <span>${transactions.description}</span>
+                <span class="transaction-amount-container">${Math.abs (transactions.amount).toFixed(2)} 
+                <span><button class="dltbtn" data-id=${transactions.id}>delete</button></span></span>
+        `;
+         this.transactionList.appendChild(transactionDiv);
+
+    });
+
+    this.attachDeleteEventListeners();
+}
+attachDeleteEventListeners(){
+    this.transactionList.querySelectorAll('.dltbtn').forEach(button=>{
+        button.addEventListener('click', ()=>{
+            this.deleteTransactions(Number(button.dataset.id));
+        })
+     });
 
 }
 deleteTransactions(id){
+this.transactions = this.transactions.filter(transaction=>transaction.id!==id);
 
-}
+this.saveTransactions();
+this.renderTransactions();
+this.updateBalance();
+};
+ 
+
 updateBalance(){
 
 }
 
 }
-const budgetTracker = new budgetTracker ()
+const budgetTracker = new BudgetTracker ();
